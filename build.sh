858fa4feac2a3ddb34424e7d7487b4add2111728
tmeralus <tedley.meralus@nasa.gov>
2024-11-06 13:23:36 -0500

    consolidate

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+# build and run dockerfile
 set -e
-
 DOCKER_RUN_IMAGE=nagios
 
 docker build -t "${DOCKER_RUN_IMAGE}" .
-
-docker images
+docker images && sleep 4
 docker run -d --rm --name "${DOCKER_RUN_IMAGE}" -p 8080:80 -t "${DOCKER_RUN_IMAGE}"
 
